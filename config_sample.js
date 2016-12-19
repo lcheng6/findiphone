395c315decb1de376e5280bbe8fe9d6360e4d42e
Liang Cheng <liangcheng6@gmail.com>
2016-12-19 02:47:39 -0500

    Add sample password

---
+++ config_sample.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.iCloudAccounts = [ 
+	{
+		"NickName": "Bob",
+		"UserName": "Bob@home.com",
+		"Password": "SamplePass"
+	}, 
+	{
+		"NickName": "Alice",
+		"UserName":"Alice@hotmail.com",
+		"Password": "SayWhat!@@#"
+	}
+];
+
+module.exports = config;
