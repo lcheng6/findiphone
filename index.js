d4387100d09f2a4132253528dc71a74b6830b4fe
Liang Cheng <liangcheng6@gmail.com>
2016-12-19 02:15:36 -0500

    First version of working account, working with one account.

--- index.js
+++ index.js
@@ -21,10 +21,16 @@
 
 'use strict';
 
-var AlexaSkill = require('./AlexaSkill'),
-    recipes = require('./recipes');
+const util = require('util');
+var AlexaSkill = require('./AlexaSkill');
+var recipes = require('./recipes');
+var icloud = require("find-my-iphone").findmyphone;
+var config = require('./config');
+var async = require('async');
 
-var APP_ID = undefined; //OPTIONAL: replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
+
+
+var APP_ID = "amzn1.ask.skill.cee844ea-1f14-4de3-89f7-a1bebe891dd9"; //OPTIONAL: replace with 'amzn1.echo-sdk-ams.app.[your-unique-value-here]';
 
 /**
  * MinecraftHelper is a child of AlexaSkill.
@@ -41,48 +47,111 @@ HowTo.prototype = Object.create(AlexaSkill.prototype);
 HowTo.prototype.constructor = HowTo;
 
 HowTo.prototype.eventHandlers.onLaunch = function (launchRequest, session, response) {
-    var speechText = "Welcome to the How To Helper. You can ask a question like, what's the recipe for a chest? ... Now, what can I help you with.";
+    var speechText = "Welcome to the iPhone Finder. Locate your iDevices with commands like, find my iPhone";
     // If the user either does not reply to the welcome message or says something that is not
     // understood, they will be prompted again with this text.
     var repromptText = "For instructions on what you can say, please say help me.";
+    console.log("onLaunch requestId="  + session.requestId + " sessionId=" + session.sessionId);
     response.ask(speechText, repromptText);
 };
 
 HowTo.prototype.intentHandlers = {
-    "RecipeIntent": function (intent, session, response) {
+    "FindiPhoneIntent": function (intent, session, response) {
         var itemSlot = intent.slots.Item,
-            itemName;
-        if (itemSlot && itemSlot.value){
-            itemName = itemSlot.value.toLowerCase();
-        }
+            itemName = itemSlot.value;
 
-        var cardTitle = "Recipe for " + itemName,
-            recipe = recipes[itemName],
-            speechOutput,
-            repromptOutput;
-        if (recipe) {
-            speechOutput = {
-                speech: recipe,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.tellWithCard(speechOutput, cardTitle, recipe);
-        } else {
-            var speech;
-            if (itemName) {
-                speech = "I'm sorry, I currently do not know the recipe for " + itemName + ". What else can I help with?";
-            } else {
-                speech = "I'm sorry, I currently do not know that recipe. What else can I help with?";
+        var cardTitle = "Looking for " + itemName;
+        var speechOutput;
+        var repromptOutput;
+
+        
+
+
+        icloud.apple_id = config.iCloudUserName;
+        icloud.password = config.iCloudPassword;
+        console.log("logging into iCloud");
+
+        icloud.getDevices(function (error, devices) {
+            var device;
+
+            if (error) {
+                console.log("icloud error: " + error);
+                throw error;
             }
-            speechOutput = {
-                speech: speech,
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            repromptOutput = {
-                speech: "What else can I help with?",
-                type: AlexaSkill.speechOutputType.PLAIN_TEXT
-            };
-            response.ask(speechOutput, repromptOutput);
-        }
+            console.log("within iCloud callback");
+            //pick a device with location and findMyPhone enabled
+            devices.forEach(function(d) {
+                
+                if (d.modelDisplayName.toLowerCase() === itemName.toLowerCase() ) {
+                    device = d;
+                    console.log("device: " + device.name);
+                }
+
+            });
+
+            if (device) {
+
+                //gets the distance of the device from my location
+  
+                var data = {
+                    "Latitude": 38.827467,
+                    "Longitude": -77.166179,
+                    "address": "4551 Sawgrass Ct. Alexandria VA, 22312"
+                };
+
+                async.waterfall([
+                    function getDeviceDistance (next) 
+                    {
+                        icloud.getDistanceOfDevice(device, data.Latitude, data.Longitude, function(err, result) {
+                            if (err) { 
+                                console.log("error locating "+ itemName + ":" + JSON.stringify(err));
+                                cardTitle = itemName + " location error";
+                                speechOutput = {
+                                    speech: "There is a problem finding your " + itemName,
+                                    type: AlexaSkill.speechOutputType.PLAIN_TEXT
+                                };
+                                response.tellWithCard(speechOutput, cardTitle, itemName)
+                                next("Failure to locate " + itemName);
+                            }else {
+                                if (result != undefined) {
+                                    console.log("Distance: " + result.distance.text);
+                                    console.log("Driving time: " + result.duration.text);
+                                }else {
+                                    console.log("Location information was given for " + itemName);
+                                }
+                                next(null, data);
+                            }
+                            
+                        });
+                    },
+                    function getDeviceLocation (data, next) {
+                        icloud.getLocationOfDevice(device, function(err, location) {
+                            console.log(location);
+                            data.Location = location;
+                            next(null, data);
+                        });
+                    },
+                    function pingDevice(data, next) {
+                        icloud.alertDevice(device.id, function(err) {
+                            console.log("Beep Beep " + JSON.stringify(device));
+
+                            speechOutput = {
+                                speech: "I'm ringing alarm on your " + itemName,
+                                type: AlexaSkill.speechOutputType.PLAIN_TEXT
+                            };
+                            response.tellWithCard(speechOutput, cardTitle, itemName);
+                            next(null, data);
+                        });
+                    }
+                ]);
+            }
+            else {
+                var speechText = itemName + " is not listed in your iCloud account, please try again";
+                var repromptText = "Try saying locate my iPod, iPhone, iPad, or MacBook Pro";
+                response.ask(speechText, repromptText)
+            }
+        });
+        
     },
 
     "AMAZON.StopIntent": function (intent, session, response) {
@@ -96,8 +165,8 @@ HowTo.prototype.intentHandlers = {
     },
 
     "AMAZON.HelpIntent": function (intent, session, response) {
-        var speechText = "You can ask questions such as, what's the recipe, or, you can say exit... Now, what can I help you with?";
-        var repromptText = "You can say things like, what's the recipe, or you can say exit... Now, what can I help you with?";
+        var speechText = "You can ask questions such as, where is my iPhone, where is my iPod, Now, what can I help you with?";
+        var repromptText = "You can say things like, where is my iPhone, or you can say exit... Now, what can I help you with?";
         var speechOutput = {
             speech: speechText,
             type: AlexaSkill.speechOutputType.PLAIN_TEXT
@@ -113,4 +182,5 @@ HowTo.prototype.intentHandlers = {
 exports.handler = function (event, context) {
     var howTo = new HowTo();
     howTo.execute(event, context);
+
 };
--- utils/package_update.sh
+++ utils/package_update.sh
@@ -1,4 +1,5 @@
 #/bin/sh
-zip -r iphonealarm.zip -xi ./node_modules config.js index.js package.json AlexaSkill.js 
+zip -r findiphone.zip -xi ./node_modules config.js index.js package.json AlexaSkill.js recipes.js
 aws lambda update-function-code --function-name findiphone --zip-file fileb://./findiphone.zip
+
 rm ./findiphone.zip
