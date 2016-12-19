2543a5e0510ce93dcc51140a10780a1b6e9e6e19
Liang Cheng <liangcheng6@gmail.com>
2016-12-19 02:56:11 -0500

    update utterance and a few other things

--- utils/package_update.sh
+++ utils/package_update.sh
@@ -1,5 +1,5 @@
 #/bin/sh
 zip -r findiphone.zip -xi ./node_modules config.js index.js package.json AlexaSkill.js recipes.js
-aws lambda update-function-code --function-name findiphone --zip-file fileb://./findiphone.zip
+aws lambda update-function-code --function-name findiPhoneMultiAccount --zip-file fileb://./findiphone.zip
 
 rm ./findiphone.zip
