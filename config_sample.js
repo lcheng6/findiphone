34b702626dc57708c60ca89d2bb6e2283a625ae6
Liang Cheng <liangcheng6@gmail.com>
2017-01-01 19:22:07 -0500

    New content for multiple accounts.

---
+++ config_sample.js
@@ -0,0 +1,16 @@
+var config = {};
+
+config.iCloudAccounts = [ 
+	{
+		"NickName": "Liang",
+		"UserName": "steve_jobs@apple.com",
+		"Password": "iphone_is_#1"
+	}, 
+	{
+		"NickName": "Kelly",
+		"UserName":"example@example.com",
+		"Password": "where_Is_mY_phone"
+	}
+];
+
+module.exports = config;
